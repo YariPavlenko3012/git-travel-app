e9b14a490a6c86f8d30dc5bff67271a75fd40bc8
Yaroslav <yari.pavlenko@gmail.com>
2022-06-28 21:12:02 +0300

    Updates

---
+++ precache-manifest.6119488f78a55fdc3fe339e208f70828.js
@@ -0,0 +1,278 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a390a5543d0c830798bd42a20e705e1b",
+    "url": "/git-travel-app/index.html"
+  },
+  {
+    "revision": "ac2e796b10ca667d24d7",
+    "url": "/git-travel-app/static/css/12.9fb20b9d.chunk.css"
+  },
+  {
+    "revision": "91b77688ee59b646b1ea",
+    "url": "/git-travel-app/static/css/13.62d17c43.chunk.css"
+  },
+  {
+    "revision": "5a987c06e16b9b4e9c7e",
+    "url": "/git-travel-app/static/css/14.62d17c43.chunk.css"
+  },
+  {
+    "revision": "743b5b3a0a6745ddcff9",
+    "url": "/git-travel-app/static/css/15.606bb817.chunk.css"
+  },
+  {
+    "revision": "68a4869685253479b7df",
+    "url": "/git-travel-app/static/css/16.606bb817.chunk.css"
+  },
+  {
+    "revision": "b43933d6de4714cb9881",
+    "url": "/git-travel-app/static/css/17.62d17c43.chunk.css"
+  },
+  {
+    "revision": "9397e4b763b0a21fcb25",
+    "url": "/git-travel-app/static/css/18.62d17c43.chunk.css"
+  },
+  {
+    "revision": "2661076ba6b1555e5f11",
+    "url": "/git-travel-app/static/css/19.62d17c43.chunk.css"
+  },
+  {
+    "revision": "b1bb749b86ba698de021",
+    "url": "/git-travel-app/static/css/20.62d17c43.chunk.css"
+  },
+  {
+    "revision": "b8b371b4a1417a008ca2",
+    "url": "/git-travel-app/static/css/21.606bb817.chunk.css"
+  },
+  {
+    "revision": "18a632d69cdfcdb6829c",
+    "url": "/git-travel-app/static/css/22.4c845bb4.chunk.css"
+  },
+  {
+    "revision": "3ec8f783a17b621976fc",
+    "url": "/git-travel-app/static/css/25.4c845bb4.chunk.css"
+  },
+  {
+    "revision": "0171ec2a0d12fe947e15",
+    "url": "/git-travel-app/static/css/26.4c845bb4.chunk.css"
+  },
+  {
+    "revision": "9c319d57357b7defced1",
+    "url": "/git-travel-app/static/css/main.c09ea6ca.chunk.css"
+  },
+  {
+    "revision": "568f3a66ad806b0080d0",
+    "url": "/git-travel-app/static/js/0.c6172a52.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/git-travel-app/static/js/0.c6172a52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "070723d0206988c27c73",
+    "url": "/git-travel-app/static/js/1.b7b3b550.chunk.js"
+  },
+  {
+    "revision": "d8c7b74d73c5d81b0f63",
+    "url": "/git-travel-app/static/js/10.e70cac5f.chunk.js"
+  },
+  {
+    "revision": "7f176a9b8b40543d32264103dc4238d9",
+    "url": "/git-travel-app/static/js/10.e70cac5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "543e74437a7064330714",
+    "url": "/git-travel-app/static/js/11.1773db66.chunk.js"
+  },
+  {
+    "revision": "7f176a9b8b40543d32264103dc4238d9",
+    "url": "/git-travel-app/static/js/11.1773db66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac2e796b10ca667d24d7",
+    "url": "/git-travel-app/static/js/12.54e6b4d4.chunk.js"
+  },
+  {
+    "revision": "1bde7cbddc37293b60694441b834365b",
+    "url": "/git-travel-app/static/js/12.54e6b4d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91b77688ee59b646b1ea",
+    "url": "/git-travel-app/static/js/13.41f11439.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/git-travel-app/static/js/13.41f11439.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a987c06e16b9b4e9c7e",
+    "url": "/git-travel-app/static/js/14.305fe866.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/git-travel-app/static/js/14.305fe866.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "743b5b3a0a6745ddcff9",
+    "url": "/git-travel-app/static/js/15.9199d375.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/git-travel-app/static/js/15.9199d375.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68a4869685253479b7df",
+    "url": "/git-travel-app/static/js/16.5b98f831.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/git-travel-app/static/js/16.5b98f831.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b43933d6de4714cb9881",
+    "url": "/git-travel-app/static/js/17.a543c8f0.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/git-travel-app/static/js/17.a543c8f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9397e4b763b0a21fcb25",
+    "url": "/git-travel-app/static/js/18.c1e28b30.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/git-travel-app/static/js/18.c1e28b30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2661076ba6b1555e5f11",
+    "url": "/git-travel-app/static/js/19.6f39077f.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/git-travel-app/static/js/19.6f39077f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dd47d4f3f07c5e42ae6",
+    "url": "/git-travel-app/static/js/2.9d9620e9.chunk.js"
+  },
+  {
+    "revision": "b1bb749b86ba698de021",
+    "url": "/git-travel-app/static/js/20.f2d0e245.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/git-travel-app/static/js/20.f2d0e245.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8b371b4a1417a008ca2",
+    "url": "/git-travel-app/static/js/21.77765fc8.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/git-travel-app/static/js/21.77765fc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18a632d69cdfcdb6829c",
+    "url": "/git-travel-app/static/js/22.77f318b2.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/git-travel-app/static/js/22.77f318b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72953fea4341dd5692a2",
+    "url": "/git-travel-app/static/js/23.14ed767a.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/git-travel-app/static/js/23.14ed767a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73e8353161d05e0b3650",
+    "url": "/git-travel-app/static/js/24.417a1397.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/git-travel-app/static/js/24.417a1397.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ec8f783a17b621976fc",
+    "url": "/git-travel-app/static/js/25.38155cf2.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/git-travel-app/static/js/25.38155cf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0171ec2a0d12fe947e15",
+    "url": "/git-travel-app/static/js/26.bd44880b.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/git-travel-app/static/js/26.bd44880b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d94279e1fb8b36df90b0",
+    "url": "/git-travel-app/static/js/27.d804fdc2.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/git-travel-app/static/js/27.d804fdc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc5a9b77829129c6fc12",
+    "url": "/git-travel-app/static/js/3.8978a320.chunk.js"
+  },
+  {
+    "revision": "54e4f94649371b62c76c",
+    "url": "/git-travel-app/static/js/4.bd2f15e6.chunk.js"
+  },
+  {
+    "revision": "48e959eaa813c1342d5e",
+    "url": "/git-travel-app/static/js/5.6467802b.chunk.js"
+  },
+  {
+    "revision": "6590f825f585a86f7c45",
+    "url": "/git-travel-app/static/js/8.97c888ee.chunk.js"
+  },
+  {
+    "revision": "7f176a9b8b40543d32264103dc4238d9",
+    "url": "/git-travel-app/static/js/8.97c888ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7477cb016cd68eba151c",
+    "url": "/git-travel-app/static/js/9.3629b022.chunk.js"
+  },
+  {
+    "revision": "7f176a9b8b40543d32264103dc4238d9",
+    "url": "/git-travel-app/static/js/9.3629b022.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c319d57357b7defced1",
+    "url": "/git-travel-app/static/js/main.b5d6062a.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/git-travel-app/static/js/main.b5d6062a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61042681007165d0256e",
+    "url": "/git-travel-app/static/js/runtime-main.beccaeb2.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/git-travel-app/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/git-travel-app/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/git-travel-app/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/git-travel-app/static/media/slick.f97e3bbf.svg"
+  }
+]);
