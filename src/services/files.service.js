d97d80cfed3f7a63b18ada852643b2f2bcf4a41e
Yaroslav <yari.pavlenko@gmail.com>
2022-06-29 19:33:13 +0300

    fix

--- src/services/files.service.js
+++ src/services/files.service.js
@@ -10,7 +10,7 @@ import {
 } from "../constants/api.constant";
 export default class FilesService {
     static async upload(data) {
-        return await axios.post("/v1/upload/files", data)
+        return await axios.post("/v1/files", data)
     }
 }
 
