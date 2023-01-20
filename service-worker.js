a01e9d6b7c1b3409c1a7e0f0ed214b91c0db77af
bruce <bruce@MacBook-Pro-bruce.local>
2023-01-20 17:56:02 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/git-travel-app/precache-manifest.1bb7e829a6fdf553935d0816a411de91.js"
+  "/git-travel-app/precache-manifest.04481ccd65fcedd23e23fe1abb8e33eb.js"
 );
 
 self.addEventListener('message', (event) => {
